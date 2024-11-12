8762695fb6c80d2456aa26679d89202c254ec259
Naveenkumar <naveen.subramaniam@aspiresys.com>
2024-11-12 11:49:38 +0530

    practice details added

---
+++ demo.py
@@ -0,0 +1,14 @@
+# Define a function to print a message
+def print_message():
+    message = "Hello, Welcome to GIT Session-Naveen kumar!"
+    print(message)
+
+# Call the function
+print_message()
+
+def print_message():
+    message = "practice: Testing and development!"
+    print(message)
+
+# Call the function
+print_message()
